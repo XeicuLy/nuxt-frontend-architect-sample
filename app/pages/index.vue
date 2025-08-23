5c9be6dd49965bbab0a7ef75b6993fecdd8b4f14
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-08-23 21:40:51 +0900

    ✨ feat: indexページ追加

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const indexPageId = useId();
+
+const greetingMessage = 'Hello, Frontend Architect Sample!';
+</script>
+<template>
+  <section :id="indexPageId">
+    <h1>{{ greetingMessage }}</h1>
+  </section>
+</template>
