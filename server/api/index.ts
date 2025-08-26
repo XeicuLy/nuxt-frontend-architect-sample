cd991c6fadcbb76f795806f4f831b3fec495af0d
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-08-26 19:07:41 +0900

    ✨ feat: OpenAPIドキュメントを生成し、SwaggerUIでドキュメント化する

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,3 +1,4 @@
+import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { healthHandler } from './routes/health';
 
@@ -7,6 +8,16 @@ const app = new OpenAPIHono().basePath('/api');
  * APIのエンドポイント追加
  */
 healthHandler(app);
+
+app.doc('/openapi.json', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'Nuxt Frontend Architect Sample API',
+  },
+});
+
+app.get('/swagger', swaggerUI({ url: '/api/openapi.json' }));
 
 export default defineEventHandler(async (event) => {
   const webReq = toWebRequest(event);
