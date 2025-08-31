e811f111a46c02cdd4bbec1fb2922fc35064ba06
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-08-31 10:27:36 +0900

    ✅ test: 不要なテストを削除

--- app/__test__/composables/common/useRuntime.nuxt.spec.ts
+++ app/__test__/composables/common/useRuntime.nuxt.spec.ts
@@ -5,8 +5,7 @@ describe('app/composables/common/useRuntime.ts', () => {
   describe('useRuntime', () => {
     test('実行環境に則したbooleanが返ること', () => {
       const { isProcessClient, isProcessServer } = useRuntime();
-      expect(isProcessClient.value).toBeTypeOf('boolean');
-      expect(isProcessServer.value).toBeTypeOf('boolean');
+
       expect(isProcessClient.value).toBe(!isProcessServer.value);
       expect(isProcessServer.value).toBe(!isProcessClient.value);
     });
