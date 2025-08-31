9f1ce93963510e53c065c40fbc52c557f5ad7d0b
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-08-31 22:05:42 +0900

    🔧 chore: vue用のプラグインを追加し、node環境でVueコンポーネントのテストができるように変更

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,6 @@
+import path from 'node:path';
 import { defineVitestProject } from '@nuxt/test-utils/config';
+import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -6,21 +8,30 @@ export default defineConfig({
     globals: true,
     reporters: ['verbose'],
     coverage: {
+      provider: 'v8',
       reportsDirectory: '../logs/coverage',
     },
     projects: [
       {
+        plugins: [vue()],
         test: {
           name: 'unit',
           include: ['**/*.{test,spec}.ts'],
-          exclude: ['**/*.nuxt.{test,spec}.ts'],
-          environment: 'node',
+          exclude: ['node_modules/', '**/*.nuxt.{test,spec}.ts'],
+          environment: 'happy-dom',
+        },
+        resolve: {
+          alias: {
+            '@': path.resolve(__dirname, './app/'),
+            '~': path.resolve(__dirname, './app/'),
+          },
         },
       },
       await defineVitestProject({
         test: {
           name: 'nuxt',
           include: ['**/*.nuxt.{test,spec}.ts'],
+          exclude: ['node_modules/'],
           environment: 'nuxt',
         },
       }),
